354c61d8f4af2e9c0b8602b0692ece4101d685a8
Daniel Mariano Varela <danielmvarela@hotmail.com>
2017-09-08 18:06:11 -0300

    Rojo
    Testeando que "transferirFondosHacia" evalue si el saldo es insuficiente

--- Test/cuentasBancarias/CuentasBancariasTest.java
+++ Test/cuentasBancarias/CuentasBancariasTest.java
@@ -9,12 +9,20 @@ public class CuentasBancariasTest {
 	public void queInicia() {
 		CuentasBancarias origen = new CuentasBancarias(2000.0);
 		CuentasBancarias destino = new CuentasBancarias();
+		Assert.assertEquals(2000.0, origen.mostrar(),0);
+		Assert.assertEquals(0.0, destino.mostrar(),0);
+	}
+
+	@Test
+	public void queEvalueLosFondos() {
+		CuentasBancarias origen = new CuentasBancarias(2000.0);
+		CuentasBancarias destino = new CuentasBancarias();
 		
-		double monto = 0;
+		double monto = 2000.1;
 
 		origen.transferirMontoHacia(monto, destino);
 		
-		Assert.assertEquals(origen.mostrar(), destino.mostrar(),0);
+		Assert.assertEquals(0.0, destino.mostrar(),0);
 	}
 
 }
